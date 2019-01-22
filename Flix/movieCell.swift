f77dd3bb8458979d2be3acf2f254acf30273186c
Seok Kwun Park <park960@purdue.edu>
2019-01-22 18:16:15 -0500

    update

---
+++ Flix/movieCell.swift
@@ -0,0 +1,24 @@
+//
+//  movieCell.swift
+//  Flix
+//
+//  Created by chris on 1/22/19.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import UIKit
+
+class movieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
